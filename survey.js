03d4d8751fc5fa5f2a8abc0f08a8a3e54b9dc028
TayrineSoares <tayrinecristina@hotmail.com>
2024-11-18 16:09:04 -0500

    Refactored with only one console.log

--- survey.js
+++ survey.js
@@ -6,13 +6,10 @@ const rl = readline.createInterface({
 });
 
 rl.question("What's your name? ", (name) => {
-  console.log(`My name is ${name}.`);
   rl.question("What's a sport you like to play? ", (sport) => {
-    console.log(`I like to play ${sport}.`); 
     rl.question("What do you listen to while doing that? ", (music) => {
-      console.log(`I like to listen to ${music} while playing ${sport}.`); 
       rl.question("Which meal is your favourite? ", (meal) => {
-        console.log(`My favourite meal is ${meal}.`); 
+        console.log(`My name is ${name}. I like to listen to ${music} while I play ${sport}. My favourite meal is ${meal}.`); 
         rl.close();
       });
     });
