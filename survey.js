e0470c9907d6fc5daab778d8853a1935d8a5f557
TayrineSoares <tayrinecristina@hotmail.com>
2024-11-18 16:02:19 -0500

    Implementing more questions

--- survey.js
+++ survey.js
@@ -5,10 +5,17 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.question("What's your name? Nicknames are also acceptable :) ", (answer) => {
-  console.log(`My name is ${answer}`);
+rl.question("What's your name? Nicknames are also acceptable :) ", (name) => {
+  console.log(`My name is ${name}`);
+  rl.question("What's an activity you like doing? ", (activity) => {
+    console.log(`I like to: ${activity}`); 
+    rl.question("What do you listen to while doing that? ", (music) => {
+      console.log(`I like to listen to: ${music} while playing ${activity}`); 
+      
+
 
   rl.close();
-});
-
+    });
+  }); 
+}); 
 
